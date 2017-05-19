933e2f277263943d5a33a9aefbdc7b10fb9adbe3
sandrojr03 <sandrojr03@github.com>
2017-05-19 09:27:40 -0300

    my first code

---
+++ novo projeto.c
@@ -0,0 +1,37 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+
+ int main (void)
+ {
+
+    int a = 7;
+    int b = 5;
+
+
+    int resp = 0;
+
+    //multiplicar
+    int i;
+    for(i=0;i<a;i++)
+    {
+        //printf("%i\n",i+1);
+
+        resp = resp + b;
+
+    }
+
+
+
+    printf("%d", resp);
+
+    //i=0;
+    //print
+
+
+
+
+
+
+    system ("pause");
+    return (0);
+ }
